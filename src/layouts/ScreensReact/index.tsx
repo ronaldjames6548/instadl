cf1583e0296fcea569ba729355d4c09e517f28ef
ronaldjames6548 <ronaldjames6548@gmail.com>
2024-09-20 18:49:32 +0500

    Update index.tsx

--- src/layouts/ScreensReact/index.tsx
+++ src/layouts/ScreensReact/index.tsx
@@ -67,25 +67,10 @@ export default function IndexHome() {
 
   return (
     <>
-<Suspense fallback={<div>Please Wait for a While...</div>}>
- <div className="w-full max-w-xl mx-auto">
-   <div className="w-full mx-auto">
-      <div className="flex items-center py-1">
-        <label className="sr-only">Search</label>
-          <form onSubmit={handleSubmit} className="flex flex-col w-full gap-2 sm:flex-row">
-            <input value={Url} onChange={(e) => setUrl(e.target.value)} aria-label="search" type="text" className="w-full px-4 rounded-md focus:ring-primary-600 focus:border-transparent focus:outline-none focus:ring-2 dark:bg-black" placeholder={`Enter ${CheckPath(Path) == "profile" ? "Username or Profile URL" : CheckPath(Path) + " URL or Paste link here"}`} required spellCheck="false" data-ms-editor="true"/>
-                            <button className="px-2 py-1 text-xs font-semibold text-gray-900 rounded-lg border shadow-md bg-gray-50" onClick={handlePaste}>Paste</button>
-   
-            <button type="submit" className="bg-current bg-pink-500  items-center py-2.5 px-3 text-sm font-medium text-white rounded-lg border focus:ring-4 focus:outline-none lg:block md:bloc hidden">DOWNLOAD</button>
-              </form>
-            </div>
-            <button type="submit" onClick={handleSubmit} className="bg-current bg-pink-500  w-full items-center py-2.5 px-3 text-sm font-medium text-white rounded-lg border focus:ring-4 focus:outline-none lg:hidden md:hidden block">DOWNLOAD</button>
-          </div>
-        </div>
-        {hide && (
-          <Download key={downloadKey} url={Url} type={CheckPath(Path)}/>
-        )}
-      </Suspense>
+
+
+
+      
     </>
   );
 }
